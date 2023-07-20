a89bfeb7fa00383f22e91e478fa3ffc3609585f1
shabeebcsbeo <shabeebcsbeo@gmail.com>
2023-07-20 20:02:30 +0530

    [BKP] repository-files: Backport from 14.0 to 13.0

--- setup/_metapackage/setup.py
+++ setup/_metapackage/setup.py
@@ -4,19 +4,19 @@ with open('VERSION.txt', 'r') as f:
     version = f.read().strip()
 
 setuptools.setup(
-    name="odoo14-addons-oca-vertical-rental",
+    name="odoo13-addons-oca-vertical-rental",
     description="Meta package for oca-vertical-rental Odoo addons",
     version=version,
     install_requires=[
-        'odoo14-addon-rental_base',
-        'odoo14-addon-rental_offday',
-        'odoo14-addon-rental_pricelist',
-        'odoo14-addon-rental_pricelist_interval',
-        'odoo14-addon-rental_product_pack',
+        'odoo13-addon-rental_base',
+        'odoo13-addon-rental_offday',
+        'odoo13-addon-rental_pricelist',
+        'odoo13-addon-rental_pricelist_interval',
+        'odoo13-addon-rental_product_pack',
     ],
     classifiers=[
         'Programming Language :: Python',
         'Framework :: Odoo',
-        'Framework :: Odoo :: 14.0',
+        'Framework :: Odoo :: 13.0',
     ]
 )
