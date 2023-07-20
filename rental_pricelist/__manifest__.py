38a9fe8c6df7ee1058d91ea1f33fae0574de4fbe
shabeebcsbeo <shabeebcsbeo@gmail.com>
2023-07-20 20:46:01 +0530

    [IMP] rental_pricelist: pre-commit changes

--- rental_pricelist/__manifest__.py
+++ rental_pricelist/__manifest__.py
@@ -6,7 +6,8 @@
     "time uom (Month, Day and Hour).",
     "version": "13.0.1.0.0",
     "category": "Rental",
-    "author": "Ametras intelligence GmbH, elego Software Solutions GmbH, Odoo Community Association (OCA)",
+    "author": "Ametras intelligence GmbH, elego Software Solutions GmbH,"
+    " Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/vertical-rental",
     "depends": [
         "rental_base",
