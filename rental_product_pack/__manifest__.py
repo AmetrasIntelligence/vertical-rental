31756ff95ed409f5995e79c80ce03a5c7f4ddb0e
shabeebcsbeo <shabeebcsbeo@gmail.com>
2023-07-20 20:47:00 +0530

    [IMP] rental_product_pack: pre-commit changes

--- rental_product_pack/__manifest__.py
+++ rental_product_pack/__manifest__.py
@@ -5,7 +5,8 @@
     "version": "13.0.1.0.0",
     "category": "Rental",
     "summary": "Manage rentals with product packs",
-    "author": "Ametras intelligence GmbH, elego Software Solutions GmbH, Odoo Community Association (OCA)",
+    "author": "Ametras intelligence GmbH, elego Software Solutions GmbH,"
+    " Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/vertical-rental",
     "depends": [
         "rental_base",
